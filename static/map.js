6eeae26f8b3b80666154e8680f5f35d9263e9cdc
Ray Ramsay <rachel.ramsay@outlook.com>
2016-08-08 16:45:40 -0700

    Directions route displays on map.

--- static/map.js
+++ static/map.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var directionsDisplay;
+// This code is based on the demo for the Google Maps lecture, an example
+// from the Google Maps JavaScript API docs, and an example from the AJAX
+// lecture.
 
-// This code is based on the demo for the Google Maps lecture and an example
-// from the Google Maps JavaScript API docs
 function initMap(){
     
     // Specify where the map is centered.
@@ -11,40 +11,52 @@ function initMap(){
     // it easier to dynamically change if you need to recenter.
     var myLatLng = {lat: 37.788668, lng: -122.411499}; // Hackbright
 
-    directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
 
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map'), {
         center: myLatLng,
-        zoom: 18,
+        zoom: 10,
     });
-}
-//
 
-function displayDirections(result) {
-    debugger;
-    directionsDisplay.setDirections(result);
     directionsDisplay.setMap(map);
-}
+    
+    var onSubmitHandler = function(evt) {
+        evt.preventDefault();
 
-// This code is based on an example from the AJAX lecture.
-function submitRequest(evt) {
-    evt.preventDefault();
+        // var formInputs = {
+        //     "start": $("#start").val(),
+        //     "end": $("#end").val(),
+        //     "departure": $("#departure").val(),
+        //     "mode": $("#mode").val()
+        // };
 
-    var formInputs = {
-        "start": $("#start").val(),
-        "end": $("#end").val(),
-        "departure": $("#departure").val(),
-        "mode": $("#mode").val()
+        // $.post("/request",
+        //       formInputs
+        //       );
+
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
     };
 
-    $.post("/request",
-          formInputs,
-          displayDirections
-          );
+    $("#directions-request").on("submit", onSubmitHandler);
 }
-//
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var mode = document.getElementById('mode').value;
+    directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: mode
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+    }
 
 google.maps.event.addDomListener(window, 'load', initMap);
-
-$("#directions-request").on("submit", submitRequest);
