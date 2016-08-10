4bb7c6d8f3cb801e19508bed21e265850cb9877b
Ray Ramsay <rachel.ramsay@outlook.com>
2016-08-10 14:38:14 -0700

    Progress in sending times to and converting times from Forecast.io API.

--- road.py
+++ road.py
@@ -2,12 +2,16 @@ import os
 import googlemaps
 import forecastio
 import pendulum
+import datetime
 import json
 
 # Remember to ``source secrets.sh``!
 
 gmaps = googlemaps.Client(key=os.environ['GOOGLE_API_SERVER_KEY'])
 fio_key = os.environ['FORECAST_API_KEY']
+
+#FIXME: I'm just for testing.
+oakcords = (37.8043637, -122.2711137)
 
 
 def get_lat_lng(loc_string):
@@ -22,8 +26,8 @@ def get_lat_lng(loc_string):
     return tup
 
 
-def format_time(time, coords):
-    """Given time as a string and a lat/lng tuple, return a timezone-aware
+def format_time(coords, time):
+    """Given time as a string and a lat/lng tuple, return a UTC
     datetime object."""
 
     timezone_result = gmaps.timezone(coords)
@@ -40,12 +44,40 @@ def jsonify_result(directions_result):
     return json.loads(directions_result)
 
 
-def get_forecast(coords):
-    """Given lat/lng tuple, return forecast."""
+def get_forecast(coords, time):
+    """Given lat/lng tuple and datetime object, return forecast."""
 
     lat = coords[0]
     lng = coords[1]
 
-    # FIXME: Add time parameter.
+    # Convert to ISO 8601 string.
+    time = time.to_iso8601_string()
 
-    return forecastio.load_forecast(fio_key, lat, lng)
+    url = 'https://api.forecast.io/forecast/%s/%s,%s,%s' \
+        % (fio_key, lat, lng, time)
+
+    return forecastio.manual(url)
+
+
+def time_checker(coords, time):
+
+    lat = coords[0]
+    lng = coords[1]
+
+    time = format_time(coords, time)
+
+    # Convert to ISO 8601 string.
+    stime = time.to_iso8601_string()
+
+    url = 'https://api.forecast.io/forecast/%s/%s,%s,%s' \
+        % (fio_key, lat, lng, stime)
+
+    forecast = forecastio.manual(url).currently()
+
+    print time
+    print forecast.time
+    print pendulum.instance(forecast.time)
+
+    print time == pendulum.instance(forecast.time)
+
+    return pendulum.instance(forecast.time)
